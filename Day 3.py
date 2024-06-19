b05ed5f6d19484d7a74d07167f6e493c56d232f1
DallasRose88 <158493720+DallasRose88@users.noreply.github.com>
2024-06-18 18:44:39 -0700

    Update Day 3.py

--- Day 3.py
+++ Day 3.py
@@ -16,3 +16,57 @@ for line in f:
         possible += 1
 
 print(possible)
+
+f = open('code','r')
+
+A = []
+B = []
+C = []
+
+for line in f:
+    split = line.strip().split(' ')
+    A.append(int(split[0]))
+    B.append(int(split[2]))
+    C.append(int(split[4]))
+
+
+possible = 0    
+x = 0
+while x < 1902:
+    count = 0
+    if A[x] + A[x+1] > A[x+2]:
+        count += 1 
+    if A[x+1] + A[x+2] > A[x]:
+        count += 1 
+    if A[x] + A[x+2] > A[x+1]:
+        count += 1 
+    if count == 3:
+        possible += 1
+    x += 3
+x = 0
+while x < 1902:
+    count = 0
+    if B[x] + B[x+1] > B[x+2]:
+        count += 1 
+    if B[x+1] + B[x+2] > B[x]:
+        count += 1 
+    if B[x] + B[x+2] > B[x+1]:
+        count += 1 
+    if count == 3:
+        possible += 1
+    x += 3
+x = 0
+while x < 1902:
+    count = 0
+    if C[x] + C[x+1] > C[x+2]:
+        count += 1 
+    if C[x+1] + C[x+2] > C[x]:
+        count += 1 
+    if C[x] + C[x+2] > C[x+1]:
+        count += 1 
+    if count == 3:
+        possible += 1
+    x += 3
+
+
+print(possible)
